f2366ceb0a567b9fff243c6e74147868210e8d8b
HLCaptain <pkblazsak@gmail.com>
2021-07-02 19:40:29 +0200

    Dots on canvas now draws faster because of optimised optimised, region based (virtual) rendering.

--- Views/CustomControls/EditorControl.xaml.cs
+++ Views/CustomControls/EditorControl.xaml.cs
@@ -28,43 +28,9 @@ namespace HyprWinUI3.Views.CustomControls {
 
 		private void canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args) {
 
-			// ini delta
-
-            // ini size
-            float dotSize = 1.6f * spaceBetweenDots / delta;
-
-
-            //args.DrawingSession.DrawText("y: " + scrollViewer.VerticalOffset.ToString(), new System.Numerics.Vector2(100, 100), Windows.UI.Color.FromArgb(255, 255, 255, 255));
-            //args.DrawingSession.DrawText("x: " + scrollViewer.HorizontalOffset.ToString(), new System.Numerics.Vector2(100, 125), Windows.UI.Color.FromArgb(255, 255, 255, 255));
-
-            // if something has changed, redraw
-            args.DrawingSession.Clear(Windows.UI.Color.FromArgb(0, 0, 0, 0));
-            for (int i = 0; i < canvas.Width / scrollViewer.ZoomFactor; i += spaceBetweenDots) {
-                for (int j = 0; j < canvas.Height / scrollViewer.ZoomFactor; j += spaceBetweenDots) {
-                    args.DrawingSession.FillCircle(new System.Numerics.Vector2(i, j), dotSize,Windows.UI.Color.FromArgb(100, 255, 255, 255));
-                }
-            }
 		}
 
-		double zoom = 0;
-		bool zooming = false;
-		private void scrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e) {
-			// todo: make canvas redraw itself loseless after zoom
-			if (zoom == scrollViewer.ZoomFactor) {
-				if (zooming) {
-                    if (needToRedraw()) {
-                        canvas.Invalidate();
-                    }
-					zooming = false;
-				}
-			} else {
-				zooming = true;
-			}
-			zoom = scrollViewer.ZoomFactor;
-			//canvas.Invalidate();
-		}
-
-        // need to redraw the dots?
+        // need to redraw the session?
         private bool needToRedraw() {
             int newSpaceBetweenDots = (int)(delta + delta * Math.Round(1 / scrollViewer.ZoomFactor / 4));
             if (spaceBetweenDots != newSpaceBetweenDots) {
@@ -72,6 +38,30 @@ namespace HyprWinUI3.Views.CustomControls {
                 return true;
             } else {
                 return false;
+            }
+        }
+
+        private void canvas_SizeChanged(object sender, SizeChangedEventArgs e) {
+            if (needToRedraw()) {
+                canvas.Invalidate();
+            }
+        }
+
+        private void canvas_RegionsInvalidated(Microsoft.Graphics.Canvas.UI.Xaml.CanvasVirtualControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasRegionsInvalidatedEventArgs args) {
+            // ini size
+            float dotSize = 1.6f * spaceBetweenDots / delta;
+
+            foreach (var region in args.InvalidatedRegions) {
+                using (var drawSession = sender.CreateDrawingSession(region)) {
+                    drawSession.Clear(Windows.UI.Color.FromArgb(0, 0, 0, 0));
+                    for (int i = 0; i < canvas.Width; i += spaceBetweenDots) {
+                        for (int j = 0; j < canvas.Height; j += spaceBetweenDots) {
+                            if (region.Contains(new Point(i, j))) {
+                                drawSession.FillCircle(new System.Numerics.Vector2(i, j), dotSize, Windows.UI.Color.FromArgb(100, 255, 255, 255));
+                            }
+                        }
+                    }
+                }
             }
         }
     }
