c95634181058f59473db56d04aac63e3be212cca
HLCaptain <pkblazsak@gmail.com>
2021-07-02 20:53:26 +0200

    Redrawing canvas further optimized, now responds to zoom changes and sets dots' size accordingly.

--- Views/CustomControls/EditorControl.xaml.cs
+++ Views/CustomControls/EditorControl.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -20,49 +21,62 @@ namespace HyprWinUI3.Views.CustomControls {
 	public sealed partial class EditorControl : UserControl {
 		const int delta = 48;
 		int spaceBetweenDots = delta;
-        
+		
 
-        public EditorControl() {
+		public EditorControl() {
 			this.InitializeComponent();
 		}
 
-		private void canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args) {
-
+		// need to redraw the session?
+		private bool needToRedraw() {
+			int newSpaceBetweenDots = (int)(delta * Math.Pow(2, Math.Round(1 / scrollViewer.ZoomFactor / 4)));
+			if (spaceBetweenDots != newSpaceBetweenDots) {
+				spaceBetweenDots = newSpaceBetweenDots;
+				return true;
+			} else {
+				return false;
+			}
 		}
 
-        // need to redraw the session?
-        private bool needToRedraw() {
-            int newSpaceBetweenDots = (int)(delta + delta * Math.Round(1 / scrollViewer.ZoomFactor / 4));
-            if (spaceBetweenDots != newSpaceBetweenDots) {
-                spaceBetweenDots = newSpaceBetweenDots;
-                return true;
-            } else {
-                return false;
-            }
-        }
+		private void canvas_RegionsInvalidated(Microsoft.Graphics.Canvas.UI.Xaml.CanvasVirtualControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasRegionsInvalidatedEventArgs args) {
+			// ini size
+			float dotSize = 2.4f * spaceBetweenDots / delta;
 
-        private void canvas_SizeChanged(object sender, SizeChangedEventArgs e) {
-            if (needToRedraw()) {
-                canvas.Invalidate();
-            }
-        }
-
-        private void canvas_RegionsInvalidated(Microsoft.Graphics.Canvas.UI.Xaml.CanvasVirtualControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasRegionsInvalidatedEventArgs args) {
-            // ini size
-            float dotSize = 1.6f * spaceBetweenDots / delta;
-
-            foreach (var region in args.InvalidatedRegions) {
-                using (var drawSession = sender.CreateDrawingSession(region)) {
-                    drawSession.Clear(Windows.UI.Color.FromArgb(0, 0, 0, 0));
-                    for (int i = 0; i < canvas.Width; i += spaceBetweenDots) {
-                        for (int j = 0; j < canvas.Height; j += spaceBetweenDots) {
-                            if (region.Contains(new Point(i, j))) {
-                                drawSession.FillCircle(new System.Numerics.Vector2(i, j), dotSize, Windows.UI.Color.FromArgb(100, 255, 255, 255));
-                            }
-                        }
+			foreach (var region in args.InvalidatedRegions) {
+				using (var drawSession = sender.CreateDrawingSession(region)) {
+					drawSession.Clear(Windows.UI.Color.FromArgb(0, 0, 0, 0));
+					for (int i = (int)Math.Round(region.Left);
+                        i < region.Right + spaceBetweenDots;
+                        i += spaceBetweenDots) {
+						for (int j = (int)Math.Round(region.Top);
+							j < region.Bottom + spaceBetweenDots;
+							j += spaceBetweenDots) {
+							drawSession.FillCircle(
+								new Vector2(
+									(float)(i - (int)Math.Round(region.Left) % spaceBetweenDots),
+									(float)(j - (int)Math.Round(region.Top) % spaceBetweenDots)),
+								dotSize,
+								Windows.UI.Color.FromArgb(100, 255, 255, 255));
+						}
+					}
+				}
+			}
+		}
+        double zoom = 0;
+        bool zooming = false;
+        private void scrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e) {
+            // todo: make canvas redraw itself loseless after zoom
+            if (zoom == scrollViewer.ZoomFactor) {
+                if (zooming) {
+                    if (needToRedraw()) {
+                        canvas.Invalidate();
                     }
+                    zooming = false;
                 }
+            } else {
+                zooming = true;
             }
+            zoom = scrollViewer.ZoomFactor;
         }
     }
 }
