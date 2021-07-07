503b322397176318d379a5c1391674bb6b1934e2
HLCaptain <pkblazsak@gmail.com>
2021-07-07 23:52:19 +0200

    Added some button functionality

--- ViewModels/EditorStartControlViewModel.cs
+++ ViewModels/EditorStartControlViewModel.cs
@@ -4,36 +4,50 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using HyprWinUI3.Services;
 using HyprWinUI3.Views.CustomControls;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using Windows.UI;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 
 namespace HyprWinUI3.ViewModels {
-    public class EditorStartControlViewModel : ObservableObject {
-        public VariableSizedWrapGrid Grid;
+	public class EditorStartControlViewModel : ObservableObject {
+		public VariableSizedWrapGrid Grid;
+
+        private ICommand _createProjectCommand;
+        private ICommand _openProjectCommand;
+
+        public ICommand CreateProjectCommand => _createProjectCommand ?? (_createProjectCommand = new RelayCommand(ProjectService.CreateProject));
+        public ICommand OpenProjectCommand => _openProjectCommand ?? (_openProjectCommand = new RelayCommand(ProjectService.OpenProject));
 
         public EditorStartControlViewModel(VariableSizedWrapGrid grid) {
-            Grid = grid;
-            ProjectService.ProjectChangedEvent += RefreshItems;
-            RefreshItems();
-        }
+			Grid = grid;
+			ProjectService.ProjectChangedEvent += RefreshItems;
+			RefreshItems();
+		}
 
-        // todo: replace dummy items with real ones
-        private void RefreshItems() {
-            Grid.Children.Clear();
-            if (ProjectService.CurrentProject == null) {
+		// todo: replace dummy items with real ones
+		private void RefreshItems() {
+			Grid.Children.Clear();
+			if (ProjectService.CurrentProject == null) {
+				Grid.Children.Add(new AppBarButton() {
+					Label = "New Project",
+					Icon = new SymbolIcon(Symbol.Add),
+                    Command = CreateProjectCommand
+				});
                 Grid.Children.Add(new AppBarButton() {
-                    Label = "New Project",
-                    Icon = new SymbolIcon(Symbol.Add)
+                    Label = "Open Project",
+                    Icon = new SymbolIcon(Symbol.OpenFile),
+                    Command = OpenProjectCommand
                 });
             }
-            Grid.Children.Add(new AppBarButton() {
-                Label = "New Class Diagram",
-                Icon = new SymbolIcon(Symbol.Add)
-            });
-        }
-    }
+			Grid.Children.Add(new AppBarButton() {
+				Label = "New Class Diagram",
+				Icon = new SymbolIcon(Symbol.Add)
+			});
+		}
+	}
 }
